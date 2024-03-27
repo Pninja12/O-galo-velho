7e961474709154c1787de652b46927184756ac4b
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-27 10:55:59 +0000

    First commit

---
+++ Executável/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Executável
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
