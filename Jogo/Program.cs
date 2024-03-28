7a24bf434f537771f989da777c534a861a31e23e
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-28 17:50:23 +0000

    Encontradas todas as peças

--- Jogo/Program.cs
+++ Jogo/Program.cs
@@ -6,7 +6,16 @@ namespace Jogo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             // Obtidos pela a ajuda do Copilot
+            char openBox = '\u25A1'; // Open box
+            char closedBox = '\u25AA'; // Closed box
+            char largerOpenBox = '\u2610'; // Larger open box
+            char largerClosedBox = '\u25A0'; // Larger closed box
+            char openCircle = '\u25CB'; // Open circle
+            char closedCircle = '\u25CF'; // Closed circle
+            char largerOpenCircle = '\u25EF'; // Larger open circle
+            char largerClosedCircle = '\u2B24'; // Larger closed circle
+
         }
     }
 }
