7143023ba74cc43cad43dbb343b7b250e8a4d0fa
Paulo Silva <pauloalmeida1288@gmail.com>
2024-04-06 21:17:52 +0100

    Correção do VerificarFim

--- Jogo/VerificarFim.cs
+++ Jogo/VerificarFim.cs
@@ -8,12 +8,17 @@ namespace Jogo
         public static bool VerificarFim(CriarPecas[] pecas)
         {
             int p1 = 0;
+            TipoPecas t1;
             int p2 = 0;
+            TipoPecas t2;
             int p3 = 0;
+            TipoPecas t3;
             int p4 = 0;
+            TipoPecas t4;
             for(int a = 0; a < 16; a++)
             {
                 p1 = pecas[a].GetPosicao();
+                t1 = pecas[a].GetTipo();
                 if (p1 == 0)
                     continue;
             for(int b = 0; b < 16; b++)
@@ -21,6 +26,7 @@ namespace Jogo
                 if (b == a)
                     continue;
                 p2 = pecas[b].GetPosicao();
+                t2 = pecas[a].GetTipo();
                 if (p2 == 0)
                     continue;
             for(int c = 0; c < 16; c++)
@@ -28,6 +34,7 @@ namespace Jogo
                 if (c == a || c == b)
                     continue;
                 p3 = pecas[c].GetPosicao();
+                t3 = pecas[a].GetTipo();
                 if (p3 == 0)
                     continue;
             for(int d = 0; d < 16; d++)
@@ -35,19 +42,94 @@ namespace Jogo
                 if (d == a || d == b || d == c)
                     continue;
                 p4 = pecas[d].GetPosicao();
+                t4 = pecas[a].GetTipo();
                 if (p4 == 0)
                     continue;
 
                 if (p1 <= 4 && p2 <= 4 && p3 <= 4 && p4 <= 4)
-                    return true;
+                    if((t1 & TipoPecas.Tamanho) == (t2 & TipoPecas.Tamanho)&&
+                    (t2 & TipoPecas.Tamanho) == (t3 & TipoPecas.Tamanho)&&
+                    (t1 & TipoPecas.Tamanho) == (t4 & TipoPecas.Tamanho)&&
+                    (t3 & TipoPecas.Tamanho) == (t2 & TipoPecas.Tamanho))
+                        return true;
+                    if((t1 & TipoPecas.Buraco) == (t2 & TipoPecas.Buraco)&&
+                    (t2 & TipoPecas.Buraco) == (t3 & TipoPecas.Buraco)&&
+                    (t1 & TipoPecas.Buraco) == (t4 & TipoPecas.Buraco)&&
+                    (t3 & TipoPecas.Buraco) == (t2 & TipoPecas.Buraco))
+                        return true;
+                    if((t1 & TipoPecas.Forma) == (t2 & TipoPecas.Forma)&&
+                    (t2 & TipoPecas.Forma) == (t3 & TipoPecas.Forma)&&
+                    (t1 & TipoPecas.Forma) == (t4 & TipoPecas.Forma)&&
+                    (t3 & TipoPecas.Forma) == (t2 & TipoPecas.Forma))
+                        return true;
+                    if((t1 & TipoPecas.Cor) == (t2 & TipoPecas.Cor)&&
+                    (t2 & TipoPecas.Cor) == (t3 & TipoPecas.Cor)&&
+                    (t1 & TipoPecas.Cor) == (t4 & TipoPecas.Cor)&&
+                    (t3 & TipoPecas.Cor) == (t2 & TipoPecas.Cor))
+                        return true;
                 if ((p1>4&&p1<9)&&(p2>4&&p2<9)&&(p3>4&&p3<9)&&(p4>4&&p4<9))
-                    return true;
+                    if((t1 & TipoPecas.Tamanho) == (t2 & TipoPecas.Tamanho)&&
+                    (t2 & TipoPecas.Tamanho) == (t3 & TipoPecas.Tamanho)&&
+                    (t1 & TipoPecas.Tamanho) == (t4 & TipoPecas.Tamanho)&&
+                    (t3 & TipoPecas.Tamanho) == (t2 & TipoPecas.Tamanho))
+                        return true;
+                    if((t1 & TipoPecas.Buraco) == (t2 & TipoPecas.Buraco)&&
+                    (t2 & TipoPecas.Buraco) == (t3 & TipoPecas.Buraco)&&
+                    (t1 & TipoPecas.Buraco) == (t4 & TipoPecas.Buraco)&&
+                    (t3 & TipoPecas.Buraco) == (t2 & TipoPecas.Buraco))
+                        return true;
+                    if((t1 & TipoPecas.Forma) == (t2 & TipoPecas.Forma)&&
+                    (t2 & TipoPecas.Forma) == (t3 & TipoPecas.Forma)&&
+                    (t1 & TipoPecas.Forma) == (t4 & TipoPecas.Forma)&&
+                    (t3 & TipoPecas.Forma) == (t2 & TipoPecas.Forma))
+                        return true;
+                    if((t1 & TipoPecas.Cor) == (t2 & TipoPecas.Cor)&&
+                    (t2 & TipoPecas.Cor) == (t3 & TipoPecas.Cor)&&
+                    (t1 & TipoPecas.Cor) == (t4 & TipoPecas.Cor)&&
+                    (t3 & TipoPecas.Cor) == (t2 & TipoPecas.Cor))
+                        return true;
                 if ((p1>8&&p1<13)&&(p2>8&&p2<13)&&(p3>8&&p3<13)&&(p4>8&&p4<13))
-                    return true;
-                if ((p1>8&&p1<13)&&(p2>8&&p2<13)&&(p3>8&&p3<13)&&(p4>8&&p4<13))
-                    return true;
+                    if((t1 & TipoPecas.Tamanho) == (t2 & TipoPecas.Tamanho)&&
+                    (t2 & TipoPecas.Tamanho) == (t3 & TipoPecas.Tamanho)&&
+                    (t1 & TipoPecas.Tamanho) == (t4 & TipoPecas.Tamanho)&&
+                    (t3 & TipoPecas.Tamanho) == (t2 & TipoPecas.Tamanho))
+                        return true;
+                    if((t1 & TipoPecas.Buraco) == (t2 & TipoPecas.Buraco)&&
+                    (t2 & TipoPecas.Buraco) == (t3 & TipoPecas.Buraco)&&
+                    (t1 & TipoPecas.Buraco) == (t4 & TipoPecas.Buraco)&&
+                    (t3 & TipoPecas.Buraco) == (t2 & TipoPecas.Buraco))
+                        return true;
+                    if((t1 & TipoPecas.Forma) == (t2 & TipoPecas.Forma)&&
+                    (t2 & TipoPecas.Forma) == (t3 & TipoPecas.Forma)&&
+                    (t1 & TipoPecas.Forma) == (t4 & TipoPecas.Forma)&&
+                    (t3 & TipoPecas.Forma) == (t2 & TipoPecas.Forma))
+                        return true;
+                    if((t1 & TipoPecas.Cor) == (t2 & TipoPecas.Cor)&&
+                    (t2 & TipoPecas.Cor) == (t3 & TipoPecas.Cor)&&
+                    (t1 & TipoPecas.Cor) == (t4 & TipoPecas.Cor)&&
+                    (t3 & TipoPecas.Cor) == (t2 & TipoPecas.Cor))
+                        return true;
                 if (p1>12&&p2>12&&p3>12&&p4>12)
-                    return true;
+                    if((t1 & TipoPecas.Tamanho) == (t2 & TipoPecas.Tamanho)&&
+                    (t2 & TipoPecas.Tamanho) == (t3 & TipoPecas.Tamanho)&&
+                    (t1 & TipoPecas.Tamanho) == (t4 & TipoPecas.Tamanho)&&
+                    (t3 & TipoPecas.Tamanho) == (t2 & TipoPecas.Tamanho))
+                        return true;
+                    if((t1 & TipoPecas.Buraco) == (t2 & TipoPecas.Buraco)&&
+                    (t2 & TipoPecas.Buraco) == (t3 & TipoPecas.Buraco)&&
+                    (t1 & TipoPecas.Buraco) == (t4 & TipoPecas.Buraco)&&
+                    (t3 & TipoPecas.Buraco) == (t2 & TipoPecas.Buraco))
+                        return true;
+                    if((t1 & TipoPecas.Forma) == (t2 & TipoPecas.Forma)&&
+                    (t2 & TipoPecas.Forma) == (t3 & TipoPecas.Forma)&&
+                    (t1 & TipoPecas.Forma) == (t4 & TipoPecas.Forma)&&
+                    (t3 & TipoPecas.Forma) == (t2 & TipoPecas.Forma))
+                        return true;
+                    if((t1 & TipoPecas.Cor) == (t2 & TipoPecas.Cor)&&
+                    (t2 & TipoPecas.Cor) == (t3 & TipoPecas.Cor)&&
+                    (t1 & TipoPecas.Cor) == (t4 & TipoPecas.Cor)&&
+                    (t3 & TipoPecas.Cor) == (t2 & TipoPecas.Cor))
+                        return true;
                 
             }
             }
