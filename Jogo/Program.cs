7ed540ea67b1397577e7a4e1bf2e71c4523fe52c
Lee-Dias <leedias.dev@gmail.com>
2024-04-06 21:31:03 +0100

    verificarfim a funcionar dentro do program.cs

--- Jogo/Program.cs
+++ Jogo/Program.cs
@@ -24,37 +24,37 @@ namespace Jogo
             //Console.ForegroundColor = ConsoleColor.White;
 
             CriarPecas[] pecas = new CriarPecas [16];
-            pecas[0] = new CriarPecas(-1,largerOpenBox,
+            pecas[0] = new CriarPecas(0,largerOpenBox,
             TipoPecas.Cor|TipoPecas.Tamanho|TipoPecas.Buraco|TipoPecas.Forma);
-            pecas[1] = new CriarPecas(-1,openBox,
+            pecas[1] = new CriarPecas(0,openBox,
             TipoPecas.Cor|TipoPecas.Buraco|TipoPecas.Forma);
-            pecas[2] = new CriarPecas(-1,largerOpenCircle,
+            pecas[2] = new CriarPecas(0,largerOpenCircle,
             TipoPecas.Cor|TipoPecas.Tamanho|TipoPecas.Buraco);
-            pecas[3] = new CriarPecas(-1,openCircle,
+            pecas[3] = new CriarPecas(0,openCircle,
             TipoPecas.Cor|TipoPecas.Buraco);
-            pecas[4] = new CriarPecas(-1,largerClosedBox,
+            pecas[4] = new CriarPecas(0,largerClosedBox,
             TipoPecas.Cor|TipoPecas.Tamanho|TipoPecas.Forma);
-            pecas[5] = new CriarPecas(-1,closedBox,
+            pecas[5] = new CriarPecas(0,closedBox,
             TipoPecas.Cor|TipoPecas.Forma);
-            pecas[6] = new CriarPecas(-1,largerClosedCircle,
+            pecas[6] = new CriarPecas(0,largerClosedCircle,
             TipoPecas.Cor|TipoPecas.Tamanho);
-            pecas[7] = new CriarPecas(-1,closedCircle,
+            pecas[7] = new CriarPecas(0,closedCircle,
             TipoPecas.Cor);
-            pecas[8] = new CriarPecas(-1,largerOpenBox,
+            pecas[8] = new CriarPecas(0,largerOpenBox,
             TipoPecas.Tamanho|TipoPecas.Buraco|TipoPecas.Forma);
-            pecas[9] = new CriarPecas(-1,openBox,
+            pecas[9] = new CriarPecas(0,openBox,
             TipoPecas.Buraco|TipoPecas.Forma);
-            pecas[10] = new CriarPecas(-1,largerOpenCircle,
+            pecas[10] = new CriarPecas(0,largerOpenCircle,
             TipoPecas.Tamanho|TipoPecas.Buraco);
-            pecas[11] = new CriarPecas(-1,openCircle,
+            pecas[11] = new CriarPecas(0,openCircle,
             TipoPecas.Buraco);
-            pecas[12] = new CriarPecas(-1,largerClosedBox,
+            pecas[12] = new CriarPecas(0,largerClosedBox,
             TipoPecas.Tamanho|TipoPecas.Forma);
-            pecas[13] = new CriarPecas(-1,closedBox,
+            pecas[13] = new CriarPecas(0,closedBox,
             TipoPecas.Forma);
-            pecas[14] = new CriarPecas(-1,largerClosedCircle,
+            pecas[14] = new CriarPecas(0,largerClosedCircle,
             TipoPecas.Tamanho);
-            pecas[15] = new CriarPecas(-1,closedCircle,0);
+            pecas[15] = new CriarPecas(0,closedCircle,0);
 
             //tutorial
             Tutorial.TutorialIniciar(pecas);
@@ -99,7 +99,12 @@ namespace Jogo
                         m += 1;
                     }  
                     Console.WriteLine(); 
-                }       
+                }     
+                //verificar quem ganha
+                if (Fim.VerificarFim(todasPecas)){
+                    jg = false;
+                   Console.WriteLine($"player {playeratual} ganhou!!");
+                };  
                 //Pedir a peça a ser utilizada 
                 if (playeratual == 1) {
                     playeratual = 2;
@@ -182,11 +187,6 @@ namespace Jogo
                     }
                     b+=1;
                 }
-
-                if (Fim.VerificarFim(todasPecas)){
-                    jg = false;
-                   Console.WriteLine($"player {playeratual} ganhou!!");
-                };
                 pecasusadas[ronda] = aserusada;
                 ronda +=1;
 
